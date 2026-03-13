537fec625473c532c1af08762e4e194caffb34d3
Vaishu-sankar <vaishnaviya.s@gmail.com>
2026-03-13 18:44:16 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+
+from flask import Flask
+import boto3
+
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return "My AWS Web Application Running"
+
+@app.route("/s3")
+def s3():
+    s3 = boto3.client('s3')
+    buckets = s3.list_buckets()
+
+    output = ""
+
+    for bucket in buckets['Buckets']:
+        output += bucket['Name'] + "<br>"
+
+    return output
+
+app.run(host="0.0.0.0", port=80)
